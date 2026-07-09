aec4c216c81367ecef5ccd4dcc34d0ddd029303c
VorpalB666 <vorpal_blade@tuta.io>
2026-07-09 20:12:29 +0200

    Just correctet wrong spellings

--- internal/scan/scan_http.go
+++ internal/scan/scan_http.go
@@ -1,10 +1,10 @@
 package scan
 
 /* HTTP Security Checks (Basic)
-👉 Mehr “Security”-Value
+ Mehr “Security”-Value
 Checke:
 	• HTTPS vorhanden?
 	• Security Headers:
 		• Strict-Transport-Security
 		• Content-Security-Policy
-👉 Das fällt unter Web Security Analyse (typisch für Scanner-Tools) [libhunt.com] */
+ Das fällt unter Web Security Analyse (typisch für Scanner-Tools) [libhunt.com] */
