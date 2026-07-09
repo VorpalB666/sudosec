cb0fc0ebb2e47f799885b314c28ed242ea03a772
VorpalB666 <vorpal_blade@tuta.io>
2026-07-09 20:12:12 +0200

    added scan_http.go - no code yet

---
+++ internal/scan/scan_http.go
@@ -0,0 +1,10 @@
+package scan
+
+/* HTTP Security Checks (Basic)
+👉 Mehr “Security”-Value
+Checke:
+	• HTTPS vorhanden?
+	• Security Headers:
+		• Strict-Transport-Security
+		• Content-Security-Policy
+👉 Das fällt unter Web Security Analyse (typisch für Scanner-Tools) [libhunt.com] */
